25b3da43a0b2318220cf96fa1338a75147d1dd54
Tejaswi Rao <rao.thejaswi@gmail.com>
2014-11-15 22:19:39 +0530

    solved overlap graph problem

--- rosalind/4B.py
+++ rosalind/4B.py
@@ -2,16 +2,16 @@ import sys
 from common import stripLine
 
 def suffixOverlap(x, y):
-    for i in range(1,len(x)+1):
-        if i > len(y):
-            break
-        if x[-i:] == y[0:i]:
-            return True
+    k = len(x) - 1
+    if x[1:] == y[0:k]:
+        return True
     return False
 
 def overlapGraph(dnas):
     for i in range(0,len(dnas)):
-        for j in range(i+1,len(dnas)):
+        for j in range(0,len(dnas)):
+            if i == j:
+                continue
             if suffixOverlap(dnas[i], dnas[j]):
                 print '%s -> %s' % (dnas[i], dnas[j])
 
