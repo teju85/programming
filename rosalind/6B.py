29158e1ac50970477343252e2b506c4f6d01fa0c
Tejaswi Rao <rao.thejaswi@gmail.com>
2014-12-27 11:24:41 +0530

    solved num breakpoints

--- rosalind/6B.py
+++ rosalind/6B.py
@@ -2,12 +2,13 @@ import sys
 from common import stripLine
 
 def numBreakPoints(arr):
-    num = 2   # begin and end
-    for i in range(0,len(arr)-1):
-        delta = arr[i+1] - arr[i]
-        if delta < -1  or  delta > 1:
+    tmp = [0]
+    tmp += arr
+    tmp.append(len(arr)+1)
+    num = 0
+    for i in range(0,len(tmp)-1):
+        if (tmp[i+1] - tmp[i]) != 1:
             num += 1
-            print arr[i+1], arr[i], i
     return num
 
 if __name__ == '__main__':
